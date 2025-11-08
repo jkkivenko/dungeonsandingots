73f8faac9d0bfe00632c826068371ffd56b8790c
jkkiveko <jameskk2004@gmail.com>
2025-11-08 16:06:56 -0500

    Remove some unused imports in DungeonPortalBlock class

--- src/main/java/com/jkkivenko/dungeonsandingots/block/DungeonPortalBlock.java
+++ src/main/java/com/jkkivenko/dungeonsandingots/block/DungeonPortalBlock.java
@@ -1,41 +1,18 @@
 package com.jkkivenko.dungeonsandingots.block;
 
-import java.util.HashSet;
-import java.util.Iterator;
-
 import com.jkkivenko.dungeonsandingots.DungeonsAndIngots;
-import com.jkkivenko.dungeonsandingots.DungeonsAndIngotsSavedData;
-import com.jkkivenko.dungeonsandingots.dungeon.DungeonManager;
 import com.jkkivenko.dungeonsandingots.dungeon.DungeonManager;
 import com.jkkivenko.dungeonsandingots.item.ModItems;
-import com.mojang.serialization.Codec;
-
-import net.minecraft.commands.arguments.blocks.BlockInput;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
-import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
-import net.minecraft.world.level.saveddata.SavedData;
-import net.minecraft.world.level.saveddata.SavedDataType;
-import net.minecraft.world.level.storage.DimensionDataStorage;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.entity.Relative;
 
 @SuppressWarnings("null")
 public class DungeonPortalBlock extends Block {
